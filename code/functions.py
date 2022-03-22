df55bec08cf003a67720aac5d929d368e63d8dee
Waizhong <1352510038@qq.com>
2022-03-22 15:45:01 +0800

    同步core分支的进度

--- code/functions.py
+++ code/functions.py
@@ -1,11 +1,13 @@
 from PyQt5.QtWidgets import QLabel
 from decimal import Decimal
 from fractions import Fraction
+from re import match
+from typing import Union
 
 from settings import symbol_lst, symbol_lst_2, symbol_turn, num_weights
 
 
-def examineInt(num):
+def examineInt(num: Union[int, float]):
 	if float(num) % 1 == 0:
 		num = int(float(num))
 	return num
@@ -22,86 +24,133 @@ def textUpdate(string: str, label: QLabel):
 				break
 
 
-def isFormula(formula: list) -> bool:
+def isformula(formula: list) -> bool:
+	"""
+	仅支持单层算式，如'1+3.5*8/9'
+	不支持多层算式，如'1+(3.5*8.9)'
+	"""
 	start = 0
+	symbol_in = False
+
 	for i in formula:
 		start += 1
 		if start % 2 == 0:
-			if i in symbol_lst or i in symbol_lst_2:
+			if i in symbol_lst:
 				continue
 		else:
 			for j in i:
-				if not j.isdigit() and j not in symbol_lst_2:
+				if not j.isdigit() or j in symbol_turn or symbol_in:
 					break
+				elif j in symbol_lst_2:
+					symbol_in = True
 			else:
+				symbol_in = False
 				continue
 		return False
 	return True
 
 
 def compute(formula: list) -> int:
-	start = 0
-	for i in range(len(formula)):
-		if '(' in formula[i]:
-			formula[i] = formula[i].replace('(', '')
-			formula[i] = formula[i].replace(')', '')
-		elif formula[i][-1] == '.' or formula[i][-1] == '/':
-			formula[i] = formula[i][:-1]
-	result = formula[0]
-	if '/' in ''.join(formula):
-		fraction_compute = True
-	else:
-		fraction_compute = False
-	for j in formula:
-		if j in symbol_lst:
-			start += 2
-			if not fraction_compute:
-				result = eval(f'Decimal(result) {symbol_turn[j]} Decimal(formula[start])')
+	"""
+	传入formula，从左到右计算，优先计算嵌套的列表内算式。
+	"""
+	def c(num1, num2, symbol: str, bl: bool) -> Union[int, float]:
+		if bl:
+			r = eval(f'Fraction(num1) {symbol_turn[symbol]} Fraction(num2)')
+		else:
+			r = eval(f'Decimal(num1) {symbol_turn[symbol]} Decimal(num2)')
+		return r
+
+	for i in formula:
+		if type(i) == list:
+			for j in i:
+				if match('^[0-9]+/[0-9]+$', j):
+					fraction_compute = True
+					break
+				else:
+					fraction_compute = False
+		else:
+			if match('^[0-9]+/[0-9]+$', i):
+				fraction_compute = True
+				break
 			else:
-				result = eval(f'Fraction(result) {symbol_turn[j]} Fraction(formula[start])')
+				fraction_compute = False
+
+	for i in range(len(formula)):
+		if type(formula[i]) == list:
+			formula_2 = formula[i][:]
+			for j in range(len(formula[i])):
+				if formula[i][j] in symbol_lst:
+					formula_2[0] = str(c(formula_2[0], formula_2[2], formula[i][j], fraction_compute))
+					del formula_2[1:3]
+			formula[i] = formula_2[:][0]
+
+	result = formula[0]
+	start = 0
+
+	for i in range(len(formula)):
+		if formula[i] in symbol_lst:
+			start += 2
+			result = c(result, formula[start], formula[i], fraction_compute)
+
 	try:
 		return examineInt(result)
 	except ValueError:
 		return result
 
 
-def getFormula(formula_string: str) -> list:
-	symbols = ['+', '-', '×', '÷', '^']
+def get_formula(formula_string: str) -> list[str]:
+	"""
+	传入字符串，将字符串转化为列表，列表每个元素是一串数字或一个符号。
+	"""
+
+	symbols = ['+', '-', '*', ':', '^']
+	front_barckets = ['(', '[', '{']
+	back_barckets = [')', ']', '}']
 	formula_string = formula_string.replace(' ', '').replace('**', '^').replace('*', '×').replace(':', '÷').replace(
 		'//', '÷')
-	formula = []
+	raw_formula = []
 	start = 0
-
-	if '=' in formula_string:
-		idx = formula_string.index('=')
-		formula_string = formula_string[:idx]
 
 	for i in range(len(formula_string)):
 		if formula_string[i] in symbols or i == len(formula_string) - 1:
-			formula.append(formula_string[start:i])
+			raw_formula.append(formula_string[start:i])
+			raw_formula.append(formula_string[i])
 			start = i + 1
-			formula.append(formula_string[i])
 
-	if '(-' not in ''.join(formula):
+	while '' in raw_formula:
+		raw_formula.remove('')
+
+	barcket_start = 0
+	is_in_barckets = False
+	formula = []
+
+	for i in range(len(raw_formula)):
+		if raw_formula[i][0] in front_barckets:
+			barcket_start = i
+			is_in_barckets = True
+		elif raw_formula[i][-1] in back_barckets:
+			formula.append([
+				i.replace('(', '').replace(')', '').replace('[', '').replace(']', '').replace('{', '').replace(
+					'}', '').replace(':', '//').replace('^', '**')
+				for i in raw_formula[barcket_start:i+1]
+			])
+			if formula[-1][1] == '-' and formula[-1][0] == '':
+				formula[-1][0] = formula[-1][1] + formula[-1][2]
+				del formula[-1][1:3]
+				if len(formula[-1]) == 1:
+					formula[-1] = formula[-1][0]
+
+			is_in_barckets = False
+		else:
+			if not is_in_barckets:
+				formula.append(raw_formula[i].replace(':', '//').replace('^', '**'))
+
+	for i in range(len(formula)):
+		while '' in formula[i] and type(formula[i]) == list:
+			formula[i].remove('')
+
+	if type(formula[-1]) != list and formula[-2] not in symbol_lst:
 		formula[-2] += formula[-1]
 		del formula[-1]
-	else:
-		index_1 = 0
-		for j in range(len(formula)):
-			try:
-				if '(' in formula[j]:
-					index_1 = j
-				elif ')' in formula[j]:
-					index_2 = j
-					formula[index_1] = ''.join(formula[index_1:index_2 + 1])
-					index_1 += 1
-					for nope in formula[index_1:index_2 + 1]:
-						index = formula.index(nope, index_1, index_2 + 1)
-						del formula[index]
-					index_1 += 1
-			except IndexError:
-				pass
-
-	while '' in formula:
-		formula.remove('')
 	return formula
