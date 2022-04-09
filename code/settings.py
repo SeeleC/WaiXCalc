2d44aefca0cae13bf294ad8b92b59162eac25fca
Waizhong <1352510038@qq.com>
2022-04-09 20:03:31 +0800

    settings.py不再读取文件

--- code/settings.py
+++ code/settings.py
@@ -1,46 +1,4 @@
 from PyQt5.QtGui import QFont
-from json import load, dump
-from os import remove
-from numpy import load as nload
-
-try:
-	data = nload('data.npy', allow_pickle=True, fix_imports=True).item()
-	remove('data.npy')
-except FileNotFoundError:
-	try:
-		with open('data.json', 'r+', encoding='utf-8') as f:
-			data = load(f)
-	except FileNotFoundError:
-		data = {
-			'settings': {
-				'isResult': False,
-				'isInBracket': False,
-				'_floatToFraction': False,
-				'_fractionToFloat': False,
-				'_enableRecordHistory': True,
-				'language': 'en_us'
-			},
-			'formula': ['0'],
-			'history': [],
-		}
-		with open('data.json', 'w+', encoding='utf-8') as f:
-			dump(data, f)
-	else:
-		try:
-			data['settings'] = data['settings']
-		except KeyError:
-			data['settings'] = data.pop('options')
-else:
-	with open('data.json', 'w+', encoding='utf-8') as f:
-		dump(data, f)
-finally:
-	try:
-		data['settings']['language'] = data['settings']['language']
-	except KeyError:
-		data['settings']['language'] = 'en_us'
-	finally:
-		with open(f'resource/lang/{data["settings"]["language"]}.json', 'r+', encoding='utf-8') as f:
-			trans = load(f)
 
 symbol_lst = ['+', '-', '×', '÷', '^']
 symbol_lst_2 = ['+', '-', '*', ':', '^', '/', '.']
