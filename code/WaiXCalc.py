ec1e1f49ee959077bd3c82e750afda23cf36ca7a
WaiZhong <1352510038@qq.com>
2022-07-16 21:24:53 +0800

    (严重)修复分数计算时崩溃的问题

--- code/WaiXCalc.py
+++ code/WaiXCalc.py
@@ -30,7 +30,7 @@ def isformula(formula: list) -> bool:
 				continue
 		else:
 			for j in i:
-				if (not j.isdigit() and j in symbol_turn) or symbol_in:
+				if j in symbol_turn or (symbol_in and not j.isdigit()):
 					break
 				elif j in symbol_lst_2:
 					symbol_in = True
@@ -80,8 +80,11 @@ def calculate(formula: list) -> Union[Fraction, float, int]:
 		for i in f:
 			if type(i) == list:
 				return fc(i)
-			if match('^[0-9]+/[0-9]+$', i):
-				return True
+			elif match('^\d+/\d+$', i):
+				break
+		else:
+			return False
+		return True
 
 	fraction_compute = fc(formula)
 
