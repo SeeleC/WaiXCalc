37efcae226981384227bf7d641bb2d56273de7e0
SeeleC <1352510038@qq.com>
2024-03-09 19:59:08 +0800

    修复计算时出现分数导致不转换后续运算符的问题；重写verify_monomial()；删除verify_formula()

--- code/core.py
+++ code/core.py
@@ -1,22 +1,7 @@
 from decimal import Decimal, InvalidOperation  # used
-from fractions import Fraction
-from typing import Union
+from fractions import Fraction  # used
 
-from config import op_disp, op_m, op_turn, op_brac, op_calc
-
-
-def verify_formula(formula: list[str]) -> bool:
-    """
-    验证列表中的算式（get_formula()的结果）是否是calculate()可进行计算的
-    require rewrite
-    """
-    for i in range(len(formula)):
-        if (i+1) % 2 == 0 and formula[i] in op_disp:
-            continue
-        elif verify_monomial(formula[i]):
-            continue
-        return False
-    return True
+from config import op_disp, op_turn, op_brac, op_calc
 
 
 def verify_frac(frac: str) -> bool:
@@ -24,44 +9,35 @@ def verify_frac(frac: str) -> bool:
     验证字符串是否是分数(Fraction)
     """
     lst = frac.split('/')
-    if len(lst) == 2 and lst[0].isdigit() and lst[1].isdigit():  # and verify_monomial(frac)
+    if len(lst) == 2 and verify_monomial(frac):
         return True
     return False
 
 
 def verify_monomial(monomial: str) -> bool:
     """
-    验证字符串是否是整数、小数或分数 require rewrite
+    验证字符串是否是不包含乘号或除号的单项式
     """
-    symbol_frequency = {'.': 0, '/': 0}
-
-    if not monomial or monomial[-1] in op_m:
+    lst_frac = monomial.split('/')
+    if len(lst_frac) > 2:
         return False
 
-    for i in monomial:
-        if i.isdigit() or i in op_m:
-            if i in op_turn or i in op_brac:
-                return False
-            elif i in op_m:
-                if i == '/' and not verify_frac(monomial):
-                    return False
-                elif symbol_frequency['.'] > 2 or symbol_frequency['/'] > 1:
-                    return False
-                symbol_frequency[i] += 1
-        else:
+    for i in lst_frac:
+        if len(i.split('.')) > 2:
             return False
-
+        for j in i:
+            if j in [*op_brac, *op_disp, *op_calc]:
+                return False
     return True
 
 
-def calculate(formula: list) -> Union[Fraction, float, int]:
+def calculate(formula: list) -> str:
     turn_frac = False
     for j in range(len(formula)):
         if formula[j] in op_turn.keys():
             formula[j] = op_turn[formula[j]]
         elif verify_frac(formula[j]):
             turn_frac = True
-            break
 
     for i in range(len(formula)):
         if formula[i] in [*op_calc, *op_brac]:
@@ -79,8 +55,6 @@ def get_formula(string: str) -> list[str]:
     """
     传入字符串，将字符串转化为算式，每个元素是一串数字或一个符号。
     可以用()作嵌套。
-    :param string:
-    :return list[str]:
     """
     def length(lst: list):
         value = 0
